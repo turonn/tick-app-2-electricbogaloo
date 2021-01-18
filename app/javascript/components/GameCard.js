7971cd2bce32968414778ae4264badcb57b48ec1
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-01-18 18:04:52 -0500

    added a GameCard component that imports into games view

---
+++ app/javascript/components/GameCard.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const GameCard = () => {
+    return(
+        <div>
+            <h3>Home Team vs Visiting Team</h3>
+            <h2>gender, level, sport</h2>
+            <p>@location</p>
+            <p>date</p>
+        </div>
+    )
+}
+
+export default GameCard
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,14 +1,18 @@
-// Run this example by adding <%= javascript_pack_tag 'index' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
+import GameCard from '../components/GameCard'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
     document.getElementById('app'),
+  )
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <GameCard />,
+    document.getElementById('game-card'),
   )
 })
