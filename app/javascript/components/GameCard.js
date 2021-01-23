046d46962b3f12595029eb9b4a58ef13b64f9508
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-01-22 21:34:56 -0500

    Butt ton of styling to GameCard and commented out erb in Gem index

--- app/javascript/components/GameCard.js
+++ app/javascript/components/GameCard.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Icon from '@material-ui/core/Icon';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,9 +16,36 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
+  calendarTop: {
+    backgroundColor: "orange", 
+    borderRadius: "4px", 
+    border: "1px solid grey",
+    marginLeft: "40px",
+    marginRight: "40px",
+  },
+  month: {
+    fontSize: "1rem"
+  },
+  calendarCard: {
+    paddingBottom: 0 //some reason won't work...
+    // https://stackoverflow.com/questions/54236623/cant-remove-padding-bottom-from-card-content-in-material-ui
+  },
+  calendarBottom: {
+    backgroundColor: "white", 
+    borderBottomLeftRadius: "4px", 
+    borderBottomRightRadius:"4px",
+    borderTop: "1px solid grey",
+    fontSize: "3rem",
+    padding: 1
+  },
+  containerStyle: {
+    textAlign: "center",
+    backgroundColor: "lightgrey",
+    borderRadius: "4px",
+  }
 }));
 
 const GameCard = () => {
@@ -22,42 +53,49 @@ const GameCard = () => {
 
   return (
     <div className={classes.root}>
-      <Container style={{textAlign:'center', backgroundColor: 'lightgrey', borderRadius: '4px'}} maxWidth="sm">
+      <Container className={classes.containerStyle} maxWidth="sm">
         <Grid container spacing={3}>
-            <Grid item xs={6}>
-                <img src='logos/Paint_Valley.jpg' alt="logo"/>
-                <div className={classes.paper}>Paint Valley</div>
-            </Grid>
-            <Grid item xs={6}>
-                <img src='logos/Adena.png' alt="logo"/>
-                <div className={classes.paper}>Adena</div>
-            </Grid>
-            <Grid item xs={12}>
-                <div className={classes.paper}>Girls Middle School Basketball</div>
-            </Grid>
-            <Grid item xs={6}>
-                <div className={classes.paper}>
-                    <Icon>room</Icon>
-                    Paint Valley High School
-                </div>
-                <Paper className={classes.paper}>Details or advertisement here</Paper>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper>
-                    <time className={classes.paper}>
-                        Insert Datetime here
-                    </time>
-                </Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Button variant="contained" color="primary">
-                    GET TICKETS
-                </Button>
-            </Grid>
+          <Grid item xs={6}>
+            <img src="logos/Paint_Valley.jpg" alt="logo" />
+            <Typography className={classes.paper}>Paint Valley</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <img src="logos/Adena.png" alt="logo" />
+            <Typography className={classes.paper}>Adena</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography className={classes.paper}>Girls Middle School Basketball</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography className={classes.paper}>
+              <Icon>room</Icon>
+              Paint Valley High School
+            </Typography>
+            <Paper className={classes.paper}>
+              <Typography>Details or advertisement here</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+                <CardContent className={classes.calendarCard}>
+                    <div className={classes.calendarTop}>
+                        <Typography className={classes.month}>September</Typography>
+                        <Typography className={classes.calendarBottom}>20</Typography>
+                    </div>
+                    <Typography>Saturday</Typography>
+                    <Typography>7:00pm</Typography>
+                </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary">
+              GET TICKETS
+            </Button>
+          </Grid>
         </Grid>
       </Container>
     </div>
   );
-}
+};
 
-export default GameCard
+export default GameCard;
